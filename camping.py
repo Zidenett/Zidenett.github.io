335d5152cbe39bc8bfec71ce781d4f49c596038e
Zidenett <132226452+Zidenett@users.noreply.github.com>
2023-04-30 13:12:32 -0600

    inittial commit

---
+++ camping.py
@@ -0,0 +1,19 @@
+camping_list = ["tent", "sleeping bags", "water", "raspberry pi", "coffee", "knife", "ethernet cable", "flash drive", "beard oil", "marshmallows"]
+
+camp_site = ["Crystal lake", 404, 89.3, True]
+
+Chuck = camping_list[4]
+Me = camping_list[9]
+
+camping_list.insert(-1, "toilet paper")
+ 
+camping_list.insert(0, "bidet")
+
+camping_list.pop(0)
+camping_list.pop(0)
+
+
+
+
+
+print(camping_list)
